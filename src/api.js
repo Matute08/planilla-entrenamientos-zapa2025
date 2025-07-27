63d46e202ee4679f2b9f1ccfb775a937aa6fb1ee
unknown <matutegon97@gmail.com>
2025-07-27 19:51:45 -0300

    configuracion despliegue

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3001/api';
-//const API_BASE_URL = 'https://planilla-entrenamientos-zapa2025.onrender.com/api';
+//const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://planilla-entrenamientos-zapa2025.onrender.com/api';
 
 export default API_BASE_URL;
